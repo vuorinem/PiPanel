51c9f3a55a6ccd9e3440c15972820ddd2ae79f5a
Mikko Vuorinen <vuorinem@gmail.com>
2023-10-01 17:54:00 +0100

    Create intial Gpio app from code samples

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Device.Gpio;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using var controller = new GpioController();
+
+controller.OpenPin(18, PinMode.Output);
+
+bool ledOn = true;
+
+while (true)
+{
+    controller.Write(18, ledOn ? PinValue.High : PinValue.Low);
+    Thread.Sleep(1000);
+    ledOn = !ledOn;
+}
